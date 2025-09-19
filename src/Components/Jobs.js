998844751255b910c34ff7a05576cec0728265ab
sammykuria <kungukuria5724@gmail.com>
2025-09-19 07:00:18 +0300

    modifying the jobs page

--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Jobs() {
   return (
@@ -13,6 +14,41 @@ function Jobs() {
     <label className="label">Email address</label>
     <input autocomplete="off" name="Email" id="Email" className="input" type="email"/>
     <div></div></div>        </div>
+      </div>
+
+
+      <div className='jobsearchbuttons'>
+        <ul>
+            <li>
+                <button>
+                   <Link>Finance</Link> 
+                </button>
+            </li>
+
+              <li>
+                <button>
+                   <Link>HealthCare</Link> 
+                </button>
+            </li>
+
+              <li>
+                <button>
+                   <Link>Education</Link> 
+                </button>
+            </li>
+
+              <li>
+                <button>
+                   <Link>Marketing</Link> 
+                </button>
+            </li>
+
+              <li>
+                <button>
+                   <Link>Design</Link> 
+                </button>
+            </li>
+        </ul>
       </div>
 
 
